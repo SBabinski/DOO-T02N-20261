28f6a8604e9cfe8a10bef8065f96c0e9991a4e6f
Fabio Bernardi Vaz <fabiotiana15@outlook.com>
2026-02-25 20:59:52 -0300

    Cria atividade 01

--- Alunos/FabioBernardiVaz/Aula 01/App.java
+++ Alunos/FabioBernardiVaz/Aula 01/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Fabio Bernardi Vaz");
+        System.out.println("Fabio Bernardi Vaz, Hello World!");
     }
 }
