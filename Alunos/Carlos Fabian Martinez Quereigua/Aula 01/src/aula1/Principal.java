a0e9004b8bd7e066fc913f0f4d084f9c470689c5
Carlosq26 <cfmquereigua@minha.fag.edu.br>
2026-02-25 21:02:36 -0300

    Imprime nome.

---
+++ Alunos/Carlos Fabian Martinez Quereigua/Aula 01/src/aula1/Principal.java
@@ -0,0 +1,8 @@
+package aula1;
+
+public class Principal {
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+		System.out.println("Carlo Fabian Martinez Queregua");
+	}
+}
